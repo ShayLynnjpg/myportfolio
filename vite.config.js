0c19be27d658ac54078ac7eab470ee28a782ec65
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2025-05-27 14:55:00 +0100

    Prepare for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myportfolio/', // your GitHub repo name with slashes
+  base: '/',  // Deploying at root, no subfolder prefix needed
 })
+
