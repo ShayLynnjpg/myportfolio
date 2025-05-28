c438b88d16a5992663396dc242c2ab4b0714d2b7
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2025-05-28 22:50:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -256,7 +256,7 @@ export default function App() {
               🎉 Portfolio updated! Check out my latest projects below ↓ &nbsp;
               • &nbsp; 🎨 Designs for print, web, and motion graphics &nbsp; •
               &nbsp; 🖥️ My other new website Finchie Design is now live —
-              Explore our latest creative work and services! - Find out me on
+              Explore our latest creative work and services! - Find out more on
               Instagram too at @FinchieDesigns
             </span>
           </div>
